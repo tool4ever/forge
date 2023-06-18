5d06358948358cb8c41db40fa8351f9cca74524b
Hans Mackowiak <hanmac@gmx.de>
2023-06-18 12:16:23 +0200

    GameCopier: setLayerTimestamp

--- forge-ai/src/main/java/forge/ai/simulation/GameCopier.java
+++ forge-ai/src/main/java/forge/ai/simulation/GameCopier.java
@@ -263,6 +263,7 @@ public class GameCopier {
         for (Card card : origGame.getCardsIn(ZoneType.Battlefield)) {
             Card otherCard = cardMap.get(card);
             otherCard.setGameTimestamp(card.getGameTimestamp());
+            otherCard.setLayerTimestamp(card.getLayerTimestamp());
             otherCard.setSickness(card.hasSickness());
             otherCard.setState(card.getCurrentStateName(), false);
             if (card.isAttachedToEntity()) {
